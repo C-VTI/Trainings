958474e4f7a2bf79f58bdcef67e10cd8b980e1d9
VTI <38349219+C-VTI@users.noreply.github.com>
2018-08-28 23:10:00 +0200

    Add files via upload

---
+++ OdeToFood/OdeToFood/Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    [Route("company/[controller]/[action]")]
+    public class AboutController
+    {
+        [Route("")]
+        public string Phone ()
+        {
+            return "1+555+555+555";
+        }
+
+        public string Address()
+        {
+            return "USA";
+        }
+    }
+}
